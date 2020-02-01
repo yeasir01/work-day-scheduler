72eaf87a944705a51e2a40203282b03e973c4d96
Yeasir H <yeasir01@gmail.com>
2020-01-31 23:51:45 -0800

    initial code commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+//import html element currentDay ID & Set Moment to current day & Time
+const hourEl = $('.hour')
+const saveBtnEl = $('.fa-save')
+
+
+var now = moment();
+var today = moment().format('LLLL');
+
+
+$('#currentDay').text(today); //Outputs todays date in jumbotron
+
+
+
+function past(){
+
+}
+
+function present(){
+
+}
+
+function future(){
+
+}
+
+function updateTime(){
+
+}
